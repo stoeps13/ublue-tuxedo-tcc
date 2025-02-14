4aabb66a12732af8f723fb818b933c7aa76e80e6
BrickMan240 <n.yigitorhan@gmail.com>
2025-02-14 08:20:26 +0300

    change base image for bazzite through sed macro

---
+++ build-bazzite.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+set -ouex pipefail
+
+RELEASE="$(rpm -E %fedora)"
+
+
+### Install packages
+
+# Packages can be installed from any enabled yum repo on the image.
+# RPMfusion repos are available by default in ublue main images
+# List of rpmfusion packages can be found here:
+# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
+
+# this installs a package from fedora repos
+rpm-ostree install screen
+
+#Exec perms for symlink script
+chmod +x /usr/bin/fixtuxedo
+#And autorun
+systemctl enable /etc/systemd/system/fixtuxedo.service
+
+
+#Hacky workaround to make TCC install elsewhere
+mkdir -p /usr/share
+rm /opt
+ln -s /usr/share /opt
+
+rpm-ostree install tuxedo-control-center
+
+cd /
+rm /opt
+ln -s var/opt /opt
+ls -al /
+
+rm /usr/bin/tuxedo-control-center
+ln -s /usr/share/tuxedo-control-center/tuxedo-control-center /usr/bin/tuxedo-control-center
+
+sed -i 's|/opt|/usr/share|g' /etc/systemd/system/tccd.service
+sed -i 's|/opt|/usr/share|g' /usr/share/applications/tuxedo-control-center.desktop
+
+systemctl enable tccd.service
+
+systemctl enable tccd-sleep.service
+
+# this would install a package from rpmfusion
+# rpm-ostree install vlc
+
+#### Example for enabling a System Unit File
+
+
+systemctl enable podman.socket
